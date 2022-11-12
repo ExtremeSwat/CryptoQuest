92eea88fe6c5bfcd06b00b59c3ee7a98e177ab38
Edward Cristi Butoiu <butoiu.edwardcristi@gmail.com>
2022-11-12 17:04:38 +0200

    added some entities

---
+++ CryptoQuestService/Models/Tableland/MapSkinsTable.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CryptoQuestService.Models.Tableland
+{
+    public class MapSkinsTable
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("skinName")]
+        public string SkinName { get; set; }
+
+        [JsonPropertyName("imagePreviewUrl")]
+        public string ImagePreviewUrl { get; set; }
+
+        [JsonPropertyName("mapUri")]
+        public string MapUri { get; set; }
+    }
+}
---
+++ CryptoQuestService/Models/Tableland/UsersTable.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoQuestService.Models.Tableland
+{
+    public class UsersTable
+    {
+        public string UserAddress { get; set; }
+        public string NickName { get; set; }
+        public int RegisteredDate { get; set; }
+    }
+}
