1d2afc7c9095c42e9791dc04a2d1d797522d1e14
Edward Cristi Butoiu <butoiu.edwardcristi@gmail.com>
2022-11-14 22:37:42 +0200

    removed unneccessary attrs

--- CryptoQuestService/Models/Tableland/Entities/MapSkinsTable.cs
+++ CryptoQuestService/Models/Tableland/Entities/MapSkinsTable.cs
@@ -1,19 +1,10 @@
-﻿using System.Text.Json.Serialization;
-
-namespace CryptoQuestService.Models.Tableland.Entities
+﻿namespace CryptoQuestService.Models.Tableland.Entities
 {
     public class MapSkinsTable
     {
-        [JsonPropertyName("id")]
         public int Id { get; set; }
-
-        [JsonPropertyName("skinName")]
         public string SkinName { get; set; }
-
-        [JsonPropertyName("imagePreviewUrl")]
         public string ImagePreviewUrl { get; set; }
-
-        [JsonPropertyName("mapUri")]
         public string MapUri { get; set; }
     }
 }
